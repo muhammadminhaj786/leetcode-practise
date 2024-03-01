ade7b26c272ad5d066c756a6d7282ddf64f682de
Muhammad Minhaj Wahid <122244089+muhammadminhaj786@users.noreply.github.com>
2024-03-01 10:40:50 -0800

    'done'

--- app.js
+++ app.js
@@ -81,17 +81,43 @@
 // filter_list(l)
 
 
-const num1 = 132189 
+// const num1 = 132189 
 
-const digitalChange = (num1) =>{
-  let str = num1.toString()
-  let num2 = 0
-  const splitStr =str.split('')
-  for (var i =0 ; i< splitStr.length;i++){
-   num2 =num2 + parseInt(splitStr[i])
+// const digitalChange = (num1) =>{
+//   let str = num1.toString()
+//   let num2 = 0
+//   const splitStr =str.split('')
+//   for (var i =0 ; i< splitStr.length;i++){
+//    num2 =num2 + parseInt(splitStr[i])
 
-  }
-  console.log(num2)
+//   }
+//   console.log(num2)
+// }
+
+// digitalChange(num1)
+
+// You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+// #Examples:
+// Kata.getMiddle("test") should return "es"
+
+const str = 'tes'
+var newlen = Math.floor(str.length / 2)
+console.log(newlen)
+var newArr;
+if(str.length % 2==0){
+  // console.log(str[newlen-1])
+  // console.log(str[newlen])
+  newArr = (str[newlen-1] + str[newlen])
+  console.log(newArr)
+
+}else{
+  // console.log('odd')
+  newArr = str[newlen]
+  console.log(newArr)
 }
 
-digitalChange(num1)
+// Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+// ['a','b','c','d','f'] -> 'e'
+
+// const arr = ['a', 'b', 'c', 'd']
+
